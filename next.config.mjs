4e6052d6b65719af144c56acdb3dae7378613fef
Artem <Actionartem@gmail.com>
2025-10-21 13:45:42 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',              // КЛЮЧ: генерим статические HTML
-  images: { unoptimized: true }, // чтобы <Image /> работал в статике
-  typescript: { ignoreBuildErrors: true },
-};
-export default nextConfig;
+  output: 'export',
+  images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true }
+}
+export default nextConfig
